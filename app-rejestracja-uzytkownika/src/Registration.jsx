7d8a7090f75006d7dc6b816d0ccfc7ae42a6ba37
Lokatex <80033919+Lokatex@users.noreply.github.com>
2024-06-28 19:46:07 +0200

    nowe

--- app-rejestracja-uzytkownika/src/Registration.jsx
+++ app-rejestracja-uzytkownika/src/Registration.jsx
@@ -2,22 +2,51 @@ import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import './Registration.css';
 
 class Registration extends Component {
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.registerUser(event.target.username.value, event.target.password.value);
+    }
+
+    registerUser(username, password){
+        fetch('http://localhost:8000/users', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            })
+        }).then(function(response) {
+            if (response.status === 200) {
+                console.log("User registered!");
+            } else {
+               console.log("User not registered"); 
+            }
+        }).catch(function(error) {
+            console.log("error!");
+        });
+    }
+
     render() {
         return <div className="Register">
-            <Form>
-                <Form.Group controId="username" size="lg">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control autofocus name="username" />
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group controlId="username" size="lg">
+                    <Form.Label className="username" >Username</Form.Label>
+                    <Form.Control autoFocus name="username" />
                 </Form.Group>
 
-                <Form.Group controId="password" size="lg">
-                    <Form.Label>Password</Form.Label>
+                <Form.Group controlId="password" size="lg">
+                    <Form.Label className="password">Password</Form.Label>
                     <Form.Control type="password" name="password" />
                 </Form.Group>
 
-                <Button block={true} size="lg" type="submit">Register</Button>
+                <Button style={{ width: '100%', marginTop: '10px' }} size="lg" type="submit">Register</Button>
             </Form>
         </div>
     }
@@ -25,4 +54,4 @@ class Registration extends Component {
 
 
 
-export default Registration 
+export default Registration;
