d5e48a34e2c18fbe02a33c65a8525c1ca1dc116e
Lokatex <80033919+Lokatex@users.noreply.github.com>
2024-06-28 14:11:30 +0200

    Naprawy

--- app-rejestracja-uzytkownika/src/Registration.jsx
+++ app-rejestracja-uzytkownika/src/Registration.jsx
@@ -1,8 +1,25 @@
 import React, {Component} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 class Registration extends Component {
     render() {
-        return "Register"
+        return <div className="Register">
+            <Form>
+                <Form.Group controId="username" size="lg">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control autofocus name="username" />
+                </Form.Group>
+
+                <Form.Group controId="password" size="lg">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" />
+                </Form.Group>
+
+                <Button block={true} size="lg" type="submit">Register</Button>
+            </Form>
+        </div>
     }
 }
 
